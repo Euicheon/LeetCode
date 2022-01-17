42c3345b1822228513999d729f13263725ee81f3
Euicheon <46182190+Euicheon@users.noreply.github.com>
2022-01-17 22:57:57 +0900

    Time: 57 ms (5.58%), Space: 14.3 MB (10.71%) - LeetHub

---
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
@@ -0,0 +1,28 @@
+from collections import Counter
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        nums = []
+        curr = head
+        while curr.next != None:
+            nums.append(curr.val)
+            curr = curr.next
+        nums.append(curr.val)
+        flag = nums[len(nums)//2]
+        curr = head
+        nums_dict = Counter(nums[:len(nums)//2+1])
+        flag_cnt = nums_dict[flag]
+        # print(nums_dict)
+        # print(flag, flag_cnt)
+        while True:
+            if curr.val == flag:
+                flag_cnt = flag_cnt - 1
+                if(flag_cnt == 0):
+                    return curr
+            curr = curr.next
+            if(curr == None):
+                return head
