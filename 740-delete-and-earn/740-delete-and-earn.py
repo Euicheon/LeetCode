9c10b8d9fbb765b8694114c349ee12fd9faa3cb8
Euicheon <46182190+Euicheon@users.noreply.github.com>
2022-03-10 20:11:45 +0900

    Time: 232 ms (7.11%), Space: 21.9 MB (8.60%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.py
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -2,11 +2,10 @@ class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
         # [2|4] [3|9] [4|4] [5|10] [6|24]
         # max(4+f[2:],9+f[3:])
-        memo = defaultdict(int)
+        memo = {}
         max_num = max(nums)
-        for num in nums:
-            memo[num] = num + memo[num]
-            # max_num = max(max_num, num)
+        for i in range(max_num):
+            memo[i+1] = nums.count(i+1)*(i+1)
         @cache
         def max_f(num):
             if(num == 1):
