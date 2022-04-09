8be080f17cea7f513b7577f14b66a912488dd839
Euicheon <46182190+Euicheon@users.noreply.github.com>
2022-04-09 23:15:30 +0900

    Update 746-min-cost-climbing-stairs.py
    Add my own solution manually.

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,19 +1,9 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        # minCost(2) = min(minCost(0)+cost[2], cost[1])
-        if(len(cost) < 3):
-            return min(cost)
-        
-        # The array's length should be 1 longer than the length of cost
-        # This is because we can treat the "top floor" as a step to reach
-        minimum_cost = [0] * (len(cost) + 1)
-        
-        # Start iteration from step 2, since the minimum cost of reaching
-        # step 0 and step 1 is 0
-        for i in range(2, len(cost) + 1):
-            take_one_step = minimum_cost[i - 1] + cost[i - 1]
-            take_two_steps = minimum_cost[i - 2] + cost[i - 2]
-            minimum_cost[i] = min(take_one_step, take_two_steps)
-
-        # The final element in minimum_cost refers to the top floor
-        return minimum_cost[-1]
+        temp = [0 for i in range(len(cost)+1)]
+        temp[0] = 0
+        temp[1] = 0
+        for i in range(2,len(cost)+1):
+            # print(cost[i-1],temp[i-1],cost[i-2],temp[i-2])
+            temp[i] = min(cost[i-1]+temp[i-1],cost[i-2]+temp[i-2])
+        return temp[-1]
