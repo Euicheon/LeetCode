3e508736597c9195587b265bf1b5b7967b73223e
Euicheon <46182190+Euicheon@users.noreply.github.com>
2022-04-15 22:40:27 +0900

    Time: 43 ms (59.39%), Space: 13.8 MB (76.41%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def sub_rob(nums: List[int]) -> int:
+            if len(nums) < 3:
+                return max(nums)
+
+            dp_table = [-1 for _ in range(len(nums))]
+            dp_table[-1], dp_table[-2] = nums[-1], max(nums[-2],nums[-1])
+            for i in range(3,len(nums)+1):
+                dp_table[-i] = max(dp_table[-i+1],dp_table[-i+2]+nums[-i])
+            # print(dp_table)
+            return dp_table[0]
+        if(len(nums) == 1): return nums[0]
+        return max(sub_rob(nums[1:]),sub_rob(nums[:-1]))
